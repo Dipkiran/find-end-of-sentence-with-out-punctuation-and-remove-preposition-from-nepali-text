32e75cd642a2a0a15e1e9f561e6a199a6c564085
Dipkiran <071bct516dip@pcampus.edu.np>
2018-05-31 11:28:33 +0545

    hello

--- english.py
+++ english.py
@@ -1,18 +1,34 @@
 import nltk
 import random
-from nltk.corpus import movie_reviews
 from nltk import bigrams
 from nltk import word_tokenize
 from nltk import sent_tokenize
 import re
-f = open('english.txt')
-raw = f.read()
-text = "you and i should go to school i am fine how are you i am fine Tell me more about you you should keep going this one is very tough"
-print(text)
+from nltk.corpus import gutenberg
+raw = gutenberg.raw("burgess-busterbrown.txt")
+# print(raw)
+# sentences = re.split(r'[.?!]\s*', raw)
+sentences = sent_tokenize(raw)
+# print(sentences)
+length = len(sentences)
+final = []
+for i in range(2,length):
+    second= sentences[i].rsplit(None, 1)[-1].lower()
+    first = sentences[i].split(None, 1)[0].lower()
+    secondword = second.replace('"',"")
+    firstword = first.replace('"',"")
+    first = firstword
+    second =  secondword
+    firstword = first.replace('.',"")
+    secondword = second.replace('.',"")
+    final.append(firstword)
+    final.append(secondword)
+bi = list(bigrams(final))
+text = "i should do it He will play football he will do it himself there are some books"
+
 newtext =  word_tokenize(text)
 bigramtext = list(bigrams(newtext))
 news = word_tokenize(raw)
-bi = list(bigrams(news))
 for texts in bigramtext:
     if texts in bi:
         newtexts = text.replace(texts[0]+ ' '+texts[1],texts[0]+' . ' + texts[1])
